25e5ea1b1100cb9749ee3b23bd9dace7a8949645
Wellington Cordeiro <willy1234x1@gmail.com>
2013-03-31 18:13:50 +0000

    Updating settings.base for static files and templates.

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -68,7 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    root("assets")
+    root("assets"),
 )
 
 # List of finder classes that know how to find static files in
@@ -104,7 +104,7 @@ ROOT_URLCONF = 'microblog.urls'
 WSGI_APPLICATION = 'microblog.wsgi.application'
 
 TEMPLATE_DIRS = (
-    root("templates")
+    root("templates"),
 )
 
 DJANGO_APPS = (
