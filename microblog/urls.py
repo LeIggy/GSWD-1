9aa7a633f332fb69945b34ff17e9c78bc7dbec3f
Wellington Cordeiro <willy1234x1@gmail.com>
2013-04-01 01:22:37 +0000

    Fixing static files.

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 
 admin.autodiscover()
 
@@ -8,4 +10,11 @@ urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns(
+    (r'^static/(.*)$', 'django.views.static.serve',
+        {
+            'document_root': settings.STATIC_ROOT,
+        }),
 )
