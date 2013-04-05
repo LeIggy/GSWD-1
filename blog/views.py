92839564cccb87ddee2b6ed09af2cb04fdd05563
Wellington Cordeiro <willy1234x1@gmail.com>
2013-04-05 07:54:54 +0000

    base templates, views, and urls for blog.

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+class PublishPostsMixin(object):
+    def get_queryset(self):
+        queryset = super(PublishPostsMixin, self).get_queryset()
+        return queryset.filter(published=True)
+
+
+class PostListView(PublishPostsMixin, ListView):
+    model = Post
+
+
+class PostDetailView(PublishPostsMixin, DetailView):
+    model = Post
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
+from . import views
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', views.HomepageView.as_view(), name=''),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
